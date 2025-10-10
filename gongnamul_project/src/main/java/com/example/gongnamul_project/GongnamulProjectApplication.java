6d4fce2fa4d1a68a449151744ecb92f466ea94e7
hyunhyun0326 <hyunhyun0326@gmail.com>
2025-10-10 17:18:06 +0900

    Add files via upload

---
+++ gongnamul_project/src/main/java/com/example/gongnamul_project/GongnamulProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.gongnamul_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongnamulProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GongnamulProjectApplication.class, args);
+	}
+
+}
