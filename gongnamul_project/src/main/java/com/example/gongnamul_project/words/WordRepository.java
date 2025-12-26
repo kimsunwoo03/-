42084be2ec45559c786f909aa9b09233af1df1e1
hyunI03 <hyunhyun0326@gmail.com>
2025-12-26 17:43:48 +0900

    Delete gongnamul_project directory

--- gongnamul_project/src/main/java/com/example/gongnamul_project/words/WordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.gongnamul_project.words;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-public interface WordRepository extends JpaRepository<WordSaving, Long> {
-    Optional<WordSaving> findByWord(String word);
-}
